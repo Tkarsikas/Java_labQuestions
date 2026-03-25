76094196e774535625da63db588773be3c134083
Tommi <tommi.karsi@gmail.com>
2026-03-26 00:52:29 +0200

    week 3 Task2 done

--- Week3 labQuestions/Task02_Sets/src/App.java
+++ Week3 labQuestions/Task02_Sets/src/App.java
@@ -1,5 +1,27 @@
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //S1
+        Set<String> sStrings = new HashSet<>();
+        sStrings.add("Aurinko");
+        sStrings.add("Kuu");
+        System.out.println(sStrings);
+        
+        //S2
+        Set<Integer> sIntegers = new HashSet<>();
+        sIntegers.add(4);
+        sIntegers.add(4);
+        System.out.println(sIntegers);
+
+        //S3
+        TreeSet<Integer> tIntegers = new TreeSet<>();
+        tIntegers.add(3);
+        tIntegers.add(1);
+        tIntegers.add(2);
+        System.out.println(tIntegers);
+
     }
 }
