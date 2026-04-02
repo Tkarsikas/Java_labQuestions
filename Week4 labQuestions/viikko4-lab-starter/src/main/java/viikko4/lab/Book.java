6dfa6c6f3931d0ca2c2f7e2996c015e18a072aae
Tommi <tommi.karsi@gmail.com>
2026-04-03 01:27:42 +0300

    L6 done

--- Week4 labQuestions/viikko4-lab-starter/src/main/java/viikko4/lab/Book.java
+++ Week4 labQuestions/viikko4-lab-starter/src/main/java/viikko4/lab/Book.java
@@ -5,12 +5,25 @@ public class Book {
     private Integer year;
 
     public Book(String t, String a, Integer y) {
-        /* TODO */ this.title = t;
-        this.author = a;
-        this.year = y;
+        if(t != null ){
+            this.title = t;
+        }else{
+            throw new IllegalArgumentException("Väärä arvo! title Book");
+        }
+        if( a != null ){
+            this.author = a;    
+        }else{
+            throw new IllegalArgumentException("Väärä arvo!author Book");
+        }
+        if(y != null && y>0){  
+            this.year = y;
+        }else{
+            throw new IllegalArgumentException("Väärä arvo!year Book");
+        }
+
     }
 
     public String label() {
-        /* TODO */ return null;
+        return author+": "+title+" ("+year+")";
     }
 }
