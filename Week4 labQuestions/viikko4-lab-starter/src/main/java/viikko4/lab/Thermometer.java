4f8f684a9d27a65a3d6b2bef9098b796c1ed6b86
Tommi <tommi.karsi@gmail.com>
2026-04-03 00:35:30 +0300

    L3 done

--- Week4 labQuestions/viikko4-lab-starter/src/main/java/viikko4/lab/Thermometer.java
+++ Week4 labQuestions/viikko4-lab-starter/src/main/java/viikko4/lab/Thermometer.java
@@ -1,1 +1,17 @@
-package viikko4.lab; public class Thermometer implements Measurable { private Double celsius; public Thermometer(Double c){/*TODO*/ this.celsius=c;} public Double measure(){/*TODO*/ return null;} }
+package viikko4.lab;
+
+public class Thermometer implements Measurable {
+    private Double celsius;
+
+    public Thermometer(Double c) {
+        if(c != null){
+        this.celsius = c;
+        }else{
+            throw new IllegalArgumentException("Väärä arvo!");
+        }
+    }
+    @Override
+    public Double measure() {
+        return celsius;
+    }
+}
